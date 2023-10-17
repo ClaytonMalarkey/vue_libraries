2e1faeb13dc444bed18c38b0920b2f7f730e0d5c
ClaytonMalarkey <malarkey1985@gmail.com>
2023-10-16 18:14:19 -0600

    intial

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <!-- Remove any HelloWorld references -->
+    <AllUsers /> <!-- Add this line to use the AllUsers component -->
+  </div>
+</template>
+
+<script>
+import AllUsers from "@/components/AllUsers.vue"; // Import the AllUsers component
+
+export default {
+  name: "App",
+  components: {
+    AllUsers, // Register the AllUsers component for use in the template
+  },
+};
+</script>
---
+++ src/components/AllUsers.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <h1>All Users</h1>
+      <div v-for="user in users" :key="user.id">
+        <p>{{ user.first_name }} {{ user.last_name }}</p>
+        <p>{{ user.email }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        users: [],
+      };
+    },
+    mounted() {
+      this.fetchUsers();
+    },
+    methods: {
+      async fetchUsers() {
+        try {
+          const response = await axios.get('https://reqres.in/api/users');
+          this.users = response.data.data;
+        } catch (error) {
+          console.error('Error fetching users:', error);
+        }
+      },
+    },
+  };
+  </script>
+  
